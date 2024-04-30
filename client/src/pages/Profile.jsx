6b85ea3069d9ecddd0022f94915b89df77968f27
Bryan Schneller <bryan.schneller@gmail.com>
2024-04-30 15:30:36 -0400

    Front-end files for pages and components

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,24 @@
+// import Post from './components/Post';
+// import ProfileImage from './components/ProfileImage';
+// import PostSection from './components/PostSection';
+
+// const EditMenu = () => {
+//     return(
+
+//     )
+// }
+
+const Profile = () => {
+    return(
+        <>
+            <h1>Name</h1>
+            <ProfileImage></ProfileImage>
+            <EditMenu></EditMenu>
+            <Bio></Bio>
+            <h2>My Bubbles:</h2>
+            <PostSection></PostSection>
+        </>
+    )
+}
+
+export default Profile;
