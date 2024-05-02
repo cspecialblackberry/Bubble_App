0fe8e9510153103c42d82887f04e521503c9b5d8
Hall Family <charliesamhall@icloud.com>
2024-05-02 12:52:45 -0400

    added back in graphql to server.js

--- server/server.js
+++ server/server.js
@@ -3,22 +3,22 @@
 //DO NOT DELETE COMMENTED OUT CODE
 
 const express = require('express')
-// const { ApolloServer } = require('@apollo/server')
-// const { expressMiddleware } = require('@apollo/server/express4')
+const { ApolloServer } = require('@apollo/server')
+const { expressMiddleware } = require('@apollo/server/express4')
 const path = require('path')
 
-// const { typeDefs, resolvers } = require('./schemas')
+const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection')
 
 const PORT = process.env.PORT || 3000
 const app = express()
-// const server = new ApolloServer({
-//     typeDefs,
-//     resolvers
-// })
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
 
 const startApolloServer = async () => {
-    // await server.start()
+    await server.start()
 
     app.use(express.urlencoded({extended: false}))
     app.use(express.json())
@@ -31,11 +31,12 @@ const startApolloServer = async () => {
         })
     }
 
-    // app.use('/graphql', expressMiddleware(server))
+    app.use('/graphql', expressMiddleware(server))
 
     db.once('open', () => {
         app.listen(PORT, () => {
             console.log(`API server running on port ${PORT}`)
+            console.log(`Use GraphQL at http://localhost:${PORT}/graphql`)
         })
     })
 }
