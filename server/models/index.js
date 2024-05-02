6332ceba66587bbe874afcb7709bab3b783162d7
Hall Family <charliesamhall@icloud.com>
2024-05-01 21:13:10 -0400

    added friend request model to index

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,5 @@
 const {User} = require('./User')
 const {Post} = require('./Post')
+const {FriendRequest} = require('./FriendRequest')
 
-module.exports = {User, Post}
+module.exports = {User, Post, FriendRequest}
