899a0257017b1dc44cc4433fae2a5fd750007c9c
Hall Family <charliesamhall@icloud.com>
2024-05-02 12:20:46 -0400

    created user and response typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,24 @@
 const typeDefs = `
+type User {
+    _id: ID
+    username: String!
+    password: String!
+    name: String
+    color: String
+    bio: String
+    posts: [ID]
+    friends: [ID]
+}
 
+type Response {
+    _id: ID
+    user: [User]
+    responseText: String!
+}
+
+type Post {
+    _id
+}
 `;
 
 module.exports = typeDefs;
