8898cbd2bfdcb46be4d3405b1a481e9620830751
Yogi Spice <caseynnewman@yahoo.com>
2024-05-02 12:27:01 -0400

    separated CSS into corresponding component folders

--- client/src/components/NavTabs/index.jsx
+++ client/src/components/NavTabs/index.jsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
+import './style.css'
 
 function NavTabs() {
     const currentPage = useLocation().pathname;
--- client/src/pages/NewPost/index.jsx
+++ client/src/pages/NewPost/index.jsx
@@ -1,8 +1,28 @@
-export default function NewPost() {
-    return (
-      <>
+import React from 'react'
+import { useState } from 'react'
+import { Text, Textarea } from '@chakra-ui/react'
 
-      </>
-    );
-  }
-  
+export default function NewPost() {
+    let [value, setValue] = React.useState('')
+
+    let handleInputChange = (event) => {
+        let inputValue = event.target.value
+        setValue(inputValue)
+    }
+
+    return (
+        <>
+            <Text mb='8px'>Value: {value}</Text>
+            <Textarea
+                value={value}
+                onChange={handleInputChange}
+                placeholder='Here is a sample placeholder'
+                size='sm'
+            />
+        </>
+    )
+}
+
+
+// on submit, takes you to home/feed where you can see your recent post
+
