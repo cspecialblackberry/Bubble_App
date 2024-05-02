3cbebcaa21fcafdd9044c376f5f4b48a93d5ce2e
Bryan Schneller <bryan.schneller@gmail.com>
2024-05-01 20:14:08 -0400

    Add notes to profile page

--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -2,11 +2,31 @@
 // import ProfileImage from './components/ProfileImage';
 // import PostSection from './components/PostSection';
 
+//Hamburger Menu
 // const EditMenu = () => {
 //     return(
 
 //     )
 // }
+
+//Profile will contain:
+    //A given user ID (yours, or the person you clicked on)
+    //Boolean about whether it's your profile or not (compare user in question ID to logged-in user ID)
+    //Display:
+        //User's name
+        //User avatar
+        //User Bio
+        //User's latest "bubbles" (10?)
+        //# of friends?
+    
+    //If it's your profile:
+        //Give edit options:
+            //Edit name
+            //Edit avatar
+            //Edit mycolor
+            //Edit bio
+            //Delete my posts
+        
 
 const Profile = () => {
     return(
@@ -22,3 +42,4 @@ const Profile = () => {
 }
 
 export default Profile;
+            
