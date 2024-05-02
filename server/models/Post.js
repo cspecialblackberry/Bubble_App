37c28b7b3ab76367c44260314137f8a355cee5fc
Hall Family <charliesamhall@icloud.com>
2024-05-01 20:40:08 -0400

    combined first and last name fields on user model

---
+++ server/models/Post.js
@@ -0,0 +1,6 @@
+const { Schema, model} = require('mongoose')
+const mongoose = require('mongoose')
+
+const postSchema = new Schema ({
+    
+})
--- server/models/User.js
+++ server/models/User.js
@@ -6,18 +6,15 @@ const mongoose = require('mongoose')
 const userSchema = new Schema({
     username: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
         required: true
     },
-    firstName: {
+    name: {
         type: String,
-        required: true
-    },
-    lastName: {
-        type: String
     },
     color: {
         type: String,
