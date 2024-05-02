ba8ddabcc4f664ba50a0b72ccc922ec7d90b7294
Hall Family <charliesamhall@icloud.com>
2024-05-01 20:23:42 -0400

    created user model

---
+++ server/models/User.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String
+    },
+    color: {
+        type: String,
+        validate: {
+            validator: function (color) {
+                return /^#(?:[0-9a-fA-F]{3}){1,2}$/g.test(color)
+            },
+            message: 'Something went wrong!'
+        }
+    },
+    bio: {
+        type: String,
+        max: [150, "Bio must be less than 150 characters."]
+    },
+    posts: []
+    })
+
+    const User = model('User', userSchema)
+
+    module.exports = {User}
