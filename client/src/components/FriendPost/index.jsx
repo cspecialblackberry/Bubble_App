b75e6f167d3bdc7fe5dc99e22eb297cda01bf6a5
Bryan Schneller <bryan.schneller@gmail.com>
2024-05-02 20:51:47 -0400

    Add component JSX and hard code values and design display for profile page

--- client/src/components/FriendPost/index.jsx
+++ client/src/components/FriendPost/index.jsx
@@ -1,27 +1,37 @@
-import { Card, Avatar, CardHeader, CardBody, CardFooter, Stack } from '@chakra-ui/react';
+import { Card, Avatar, CardHeader, CardBody, CardFooter, Stack, Heading, Button, Text } from '@chakra-ui/react';
 
 const FriendPost = (props) => {
-    const { url, name, text, userId } = props;
+    const { url, name, color, text, userId } = props;
     return (
         <Card
+            className='friend-bubble'
             direction={{ base: 'column', sm: 'row' }}
             overflow='hidden'
             variant='outline'
+            border='1px'
+            borderColor={color}
+            borderRadius={35}
+            width={350}
+            display='flex'
+            alignItems="center"
+            flexDirection='row'
+            marginTop={3}
         >
-            <Avatar
-                size='md' name={name} src={{url}}
-            />
-
+            
+            <Stack>
+                <Heading size='md'>{name}</Heading>
+                <Avatar
+                    size='md' src={url}
+                />
+            </Stack>
             <Stack>
                 <CardBody>
-                    <Heading size='md'>{name}</Heading>
                     <Text py='2'>
                         {text}
                     </Text>
                 </CardBody>
-
                 <CardFooter>
-                    <Button variant='solid' colorScheme='blue'>
+                    <Button variant='solid' bgColor={color} border={0}>
                         REPLY
                     </Button>
                 </CardFooter>
--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -1,38 +1,51 @@
-// import Post from './components/Post';
+import { Card, CardHeader, CardBody, CardFooter, Text } from '@chakra-ui/react'; //for Bio section
 import UserAvatar from '../../components/ProfileImage';
-// import PostSection from './components/PostSection';
+// import EditDrawer from '../../components/EditDrawer';
+import YourPost from '../../components/YourPost';
+import FriendPost from '../../components/FriendPost';
+import './style.css';
 
 //Profile will contain:
-    //A given user ID (yours, or the person you clicked on)
-    //Boolean about whether it's your profile or not (compare user in question ID to logged-in user ID)
-    //Display:
-        //User's name
-        //User avatar
-        //User Bio
-        //User's latest "bubbles" (10?)
-        //# of friends?
-    
-    //If it's your profile:
-        //Give edit options:
-            //Edit name
-            //Edit avatar
-            //Edit mycolor
-            //Edit bio
-            //Delete my posts
-        
+//A given user ID (yours, or the person you clicked on)
+//Boolean about whether it's your profile or not (compare user in question ID to logged-in user ID)
+//Display:
+//User's name
+//User avatar
+//User Bio
+//User's "bubbles"
+
+//EDIT (If it's your profile):
+//Give edit options:
+//Edit name
+//Edit avatar
+//Edit mycolor
+//Edit bio
+//Delete my posts
+
+//Hardcoded information:
+const name = 'Jimmy Smith';
+const image = '/profile-icon.svg';
+const bio = 'I am a React coder from Cleveland, Ohio. Excited to meet more friends on this site.';
+const bubbles = ['Let\'s go Phillies! Big win tonight!', 'Stop judging people for what they put on their bagels! Chocolate sauce is good!', 'Anyone want to go to the mall later on?'];
+const color = '#B9E5FF';
+
 
 const Profile = () => {
-    return(
+    return (
         <>
-            <h1>User's full name</h1>
-            <UserAvatar></UserAvatar>
-            <EditMenu></EditMenu>
-            <Bio></Bio>
-            <h2>My Bubbles:</h2>
-            <PostSection></PostSection>
+            <h1>{name}</h1>
+            {/* <EditDrawer></EditDrawer> */}
+            <UserAvatar url={image} name={name}></UserAvatar>
+            <Text bgColor={color}>{bio}</Text>
+            <h2>Recent Bubbles:</h2>
+            {/*Needs a ternary for whether the user's id matches the logged in user's id or not*/}
+            {bubbles.map((bubble, index) => {
+                return(
+                    <YourPost key={index} name={name} url={image} text={bubble} color={color}></YourPost>
+                )
+            })}
         </>
     )
 }
 
 export default Profile;
-            
