65d63aa4a81e023ebd420f30214dc129de9f2d28
Hall Family <charliesamhall@icloud.com>
2024-05-07 13:23:38 -0400

    removed testing from friends page

--- client/src/pages/Friends/index.jsx
+++ client/src/pages/Friends/index.jsx
@@ -1,23 +1,8 @@
-import { useQuery } from "@apollo/client"
-import {QUERY_USERS} from '../../utils/queries'
-import { useState } from "react";
-
 export default function Friends() {
-    const {loading, error, data} = useQuery(QUERY_USERS)
-    const [users, setUsers] = useState([])
 
-    if (loading) return 'Loading...';
-    if (error) return `Error! ${error.message}`;
-    if(data) {console.log(data)}
+  return (
+    <>
 
-    
-
-    return (
-      <>
-          {users.map((user) => {
-            <h2>{user.username}</h2>
-          })}
-      </>
-    );
-  }
-  
+    </>
+  );
+}
