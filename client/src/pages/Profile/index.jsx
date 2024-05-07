aacf1542b6ba19919d19b7f961d78787a862ccbc
Bryan Schneller <bryan.schneller@gmail.com>
2024-05-07 12:21:32 -0400

    Add buttons to show/hide edit panel

--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -1,8 +1,10 @@
-import { Card, CardHeader, CardBody, CardFooter, Text } from '@chakra-ui/react'; //for Bio section
+import { Card, CardHeader, CardBody, CardFooter, Text, IconButton } from '@chakra-ui/react';
+import { EditIcon } from '@chakra-ui/icons';
 import UserAvatar from '../../components/ProfileImage';
 import EditForm from '../../components/EditForm';
 import YourPost from '../../components/YourPost';
 import FriendPost from '../../components/FriendPost';
+import { useState } from 'react';
 import './style.css';
 
 //Profile will contain:
@@ -17,18 +19,29 @@ import './style.css';
 
 //Hardcoded information:
 const name = 'Jimmy Smith';
-const image = '/profile-icon.svg';
+const image = '/avatarImages/forestSimon2.jpg';
 const bio = 'I am a React coder from Cleveland, Ohio. Excited to meet more friends on this site.';
 const color = '#FFD073';
 const bubbles = ['Let\'s go Phillies! Big win tonight!', 'Stop judging people for what they put on their bagels! Chocolate sauce is good!', 'Anyone want to go to the mall later on?'];
 
 const Profile = () => {
+    const [editIsOpen, setEditIsOpen] = useState(false);
+
+    const handleEditButtonClick = () => {
+        console.log('hit');
+        if(editIsOpen){
+            setEditIsOpen(false);
+        } else {
+            setEditIsOpen(true);
+        }
+    }
+
     return (
-        <>
+        <>  
             <h1>{name}</h1>
-            <EditForm></EditForm>
             <UserAvatar url={image} name={name}></UserAvatar>
             <Text bgColor={color}>{bio}</Text>
+            {editIsOpen ? <EditForm editIsOpen={editIsOpen} setEditIsOpen={setEditIsOpen}></EditForm> : <IconButton aria-label='Edit Profile' icon={<EditIcon/>} onClick={handleEditButtonClick} ></IconButton>}
             <h2>Recent Bubbles:</h2>
             {bubbles.map((bubble, index) => {
                 return(
