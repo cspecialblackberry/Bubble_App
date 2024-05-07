97d374f0ceaf525a1f9ebad5b9f0154521ed7f15
Hall Family <charliesamhall@icloud.com>
2024-05-06 23:24:26 -0400

    added todos

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
 const { User, Post, Response } = require('../models')
 const { signToken, AuthenticationError } = require('../utils/auth')
-const { Mongoose } = require('mongoose')
+const { ObjectId } = require('mongodb')
 
 const resolvers = {
     Query: {
@@ -90,9 +90,22 @@ const resolvers = {
 
 
         },
+        //not working
         deletePost: async (parent, { userId, postId }) => {
-
+            const user = await User.findById(userId)
+            console.log(user)
+            console.log(user.posts[0]._id.toString()===postId)
+            const postIndex = user.posts.indexOf(user.posts.find((obj) => obj._id.toString() === postId))
+            console.log(postIndex)
+            user.posts.splice(postIndex, 1)
+            const update = User.updateOne({
+                _id: new ObjectId(userId)
+            }, user)
+            Post.deleteOne({
+                _id: postId
+            })
         },
+        //not working
         addReply: async (parent, { postId, userId, responseText }) => {
             const post = await Post.findById(postId)
             console.log(post)
@@ -101,7 +114,9 @@ const resolvers = {
                 responseText
             })
             console.log(post)
-
+            Post.updateOne({
+                _id: postId
+            }, post)
         },
         deleteReply: async (parent, { postId, replyId }) => {
 
