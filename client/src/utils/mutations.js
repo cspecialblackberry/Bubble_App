32c82a409774141996f664565e05c01d64368ccc
Hall Family <charliesamhall@icloud.com>
2024-05-07 14:15:26 -0400

    added mutations in client end

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,6 +11,38 @@ export const ADD_USER = gql`
     }
 `
 
+export const EDIT_USER = gql`
+    mutation editUser($userId: ID!, $color: String, $bio: String, $name: String $avatar: String){
+        editUser(userId: $userId, color: $color, bio: $bio, name: $name, avatar: $avatar){
+        _id
+        color
+        name
+        bio
+        avatar
+        }
+    }
+`
+
+export const ADD_FRIEND = gql`
+    mutation addFriend($userId: ID!, $friendId: ID!){
+        addFriend(userId: $userId, friendId: $friendId){
+        _id
+        username
+        friends
+        }
+    }
+`
+
+export const REMOVE_FRIEND = gql`
+    mutation removeFriend($userId: ID!, $friendId: ID!){
+        removeFriend(userId: $userId, friendId: $friendId){
+        _id
+        username
+        friends
+        }
+    }
+`
+
 export const LOGIN = gql`
     mutation login($username:String!, $password:String!){
         login(username: $username, password: $password){
@@ -29,6 +61,26 @@ export const ADD_POST = gql`
             _id
             user
             postText
+        }
+    }
+`
+
+export const DELETE_POST = gql`
+    mutation deletePost($userId: ID!, $postId: ID!){
+        deletePost(userId: $userId, postId: $postId){
+        _id
+        }
+    }
+`
+
+export const ADD_REPLY = gql`
+    mutation addReply($postId: ID!, $userId: ID!, $responseText: String!){
+        addReply(postId: $postId, userId: $userId, responseText: $responseText){
+        _id
+        replies {
+            _id
+            responseText
+        }
         }
     }
 `
