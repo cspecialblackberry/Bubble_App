103f2f8479fd2f3789218b3f4ef58186cef8de0c
Hall Family <charliesamhall@icloud.com>
2024-05-07 12:09:10 -0400

    working on mutation bugs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,6 +4,7 @@ const { ObjectId } = require('mongodb')
 
 const resolvers = {
     Query: {
+        //all working
         users: async () => {
             return await User.find({})
         },
@@ -22,17 +23,17 @@ const resolvers = {
     },
 
     Mutation: {
+        //working
         addUser: async (parent, { username, password, name }) => {
             return await User.create({ username, password, name })
         },
-        //INCOMPLETE
+        //not working
         editUser: async (parent, args, context) => {
-            if (context.user) {
-                return await User.findByIdAndUpdate(context.user._id, { args })
-            }
+            console.log(args)
+            return await User.findByIdAndUpdate(args.userId, {$set: { ...args }})
 
-            throw AuthenticationError
         },
+        //working
         login: async (parent, { username, password }) => {
             const user = await User.findOne({ username });
 
@@ -50,6 +51,7 @@ const resolvers = {
 
             return { token, user };
         },
+        //working
         addFriend: async (parent, { userId, friendId }) => {
             try {
                 const friend = await User.findById(friendId)
@@ -58,6 +60,7 @@ const resolvers = {
                 return err
             }
         },
+        //working
         removeFriend: async (parent, { userId, friendId }) => {
             try {
                 const user = await User.findById(userId)
@@ -70,6 +73,7 @@ const resolvers = {
                 return err
             }
         },
+        //working
         addPost: async (parent, { userId, postText }, context) => {
             console.log({ userId, postText })
             const user = await User.findById(userId)
@@ -92,31 +96,37 @@ const resolvers = {
         },
         //not working
         deletePost: async (parent, { userId, postId }) => {
-            const user = await User.findById(userId)
-            console.log(user)
-            console.log(user.posts[0]._id.toString()===postId)
-            const postIndex = user.posts.indexOf(user.posts.find((obj) => obj._id.toString() === postId))
-            console.log(postIndex)
-            user.posts.splice(postIndex, 1)
-            const update = User.updateOne({
-                _id: new ObjectId(userId)
-            }, user)
-            Post.deleteOne({
-                _id: postId
-            })
+            try {
+                const user = await User.findById(userId)
+                console.log(user)
+                console.log(user.posts[0]._id.toString() === postId)
+                const postIndex = user.posts.indexOf(user.posts.find((obj) => obj._id.toString() === postId))
+                console.log(postIndex)
+                user.posts.splice(postIndex, 1)
+                console.log(user.posts)
+                const update = User.findByIdAndUpdate(userId, { posts: user.posts })
+                console.log('update', update._update)
+                Post.findByIdAndDelete(postId)
+            } catch (err) {
+                console.error(err)
+                return err
+            }
         },
         //not working
         addReply: async (parent, { postId, userId, responseText }) => {
             const post = await Post.findById(postId)
             console.log(post)
-            post.replies.push({
-                user: userId,
-                responseText
-            })
-            console.log(post)
+            // post.replies.push({
+            //     user: userId,
+            //     responseText
+            // })
+            // console.log(post)
             Post.updateOne({
                 _id: postId
-            }, post)
+            }, {$push: {replies: {
+                user: userId,
+                responseText
+            }}})
         },
         deleteReply: async (parent, { postId, replyId }) => {
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -39,7 +39,7 @@ type Query {
 
 type Mutation {
     addUser(username: String!, password: String!, name: String): User
-    editUser(name: String, color: String, bio: String, avatar: String): User
+    editUser(userId: ID!, name: String, color: String, bio: String, avatar: String): User
     addFriend(userId: ID!, friendId: ID!): User
     removeFriend(userId: ID!, friendId: ID!): User
     login(username: String!, password: String!): Auth
