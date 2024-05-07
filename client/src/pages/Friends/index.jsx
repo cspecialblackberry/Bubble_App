eca4ef618528102f867557456292c9a3e2c4658e
Yogi Spice <caseynnewman@yahoo.com>
2024-05-07 11:57:53 -0400

    working on user query route

--- client/src/pages/Friends/index.jsx
+++ client/src/pages/Friends/index.jsx
@@ -1,8 +1,25 @@
-export default function Friends() {
-    return (
-      <>
+import { useQuery } from '@apollo/client';
+import { QUERY_USERS } from '../../utils/queries';
+import { useState, useEffect } from "react";
 
-      </>
-    );
-  }
-  
+export default function Friends() {
+  const { loading, error, data } = useQuery(QUERY_USERS);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    if (data) {
+      setUsers(prevUsers => [...prevUsers, ...data.users]);
+    }
+  }, [data]);
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+
+  return (
+    <>
+      {users.map((user) => (
+        <h2 key={user._id}>{user.username}</h2>
+      ))}
+    </>
+  );
+}
