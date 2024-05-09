7e764574985991236ca491e3b8738900dd00a749
Hall Family <charliesamhall@icloud.com>
2024-05-09 15:06:30 -0400

    added filter for posts

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -2,8 +2,8 @@ import YourPost from "../../components/YourPost";
 import FriendPost from "../../components/FriendPost";
 import './style.css';
 import Auth from '../../utils/auth'
-// import {QUERY_USER, QUERY_POSTS} from '../../utils/queries';
-// import {useQuery, useLazyQuery} from '@apollo/client';
+import { QUERY_USER, QUERY_POSTS } from '../../utils/queries';
+import { useQuery, useLazyQuery } from '@apollo/client';
 import Search from "../../components/SearchBar";
 
 const name = ['Jimmy Smith', 'Marie Travolta', 'Billy Lou', 'Gren Thalamus', 'Kristine Sinclair', 'Benjamin Phonics'];
@@ -18,33 +18,42 @@ const bubbles = ['Let\'s go Phillies! Big win tonight!',
 
 export default function Home() {
 
-  // const token = Auth.getProfile()
-  // console.log(token.data._id)
+  const token = Auth.getProfile()
+  console.log(token.data._id)
 
-      // const [getUser, {called, loading, data}] = useLazyQuery(
-      //   QUERY_USER, {variables: {_id: token.data._id}}
-      // )
+  const [getUser, { called, loading, data }] = useLazyQuery(
+    QUERY_USER, { variables: { _id: token.data._id } }
+  )
 
-  // const userResult = useQuery(
-  //   QUERY_USER, {variables: {_id: token.data._id}}
-  // )
-  // if(userResult.loading)console.log('loading')
-  // if(userResult.data)console.log(userResult.data)
-  // if(userResult.error)console.log(userResult.error)
+  const userResult = useQuery(
+    QUERY_USER, { variables: { _id: token.data._id } }
+  )
+  if (userResult.loading) console.log('loading')
+  if (userResult.data) console.log(userResult.data)
+  if (userResult.error) console.log(userResult.error)
 
-  // let posts = []  
-  // // const user = data?.user
+  let posts = []
+  let homePosts = []
+  const user = userResult.data?.user
+  console.log(user)
 
-  // const postsResult = useQuery(
-  //   QUERY_POSTS
-  // )
+  const postsResult = useQuery(
+    QUERY_POSTS
+  )
 
-  // if(postsResult.data){
-  //   console.log(postsResult.data)
-  //   posts = postsResult.data
-  // }
-
-  // console.log(posts)
+  if (postsResult.data) {
+    console.log(postsResult.data)
+    posts = postsResult.data.posts
+    console.log(posts)
+    homePosts = posts.filter((post) => {
+      if(post.user === token.data._id){
+          return post
+      }else if(user.friends.includes(post.user)){
+          return post
+      }
+    })
+    console.log(homePosts)
+  }
 
   return (
     <>
