8c8469e9f12e1d7fbb7981b59a288d99ecdb78e2
Bryan Schneller <bryan.schneller@gmail.com>
2024-05-10 06:27:05 -0400

    Change profile design

--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -60,13 +60,12 @@ const Profile = () => {
                 :
                 <>
                     <Box padding={5} bgColor={userInfo.data.user.color} marginBottom={5} borderRadius='50%'>
-                    {hasEditButton ? editIsOpen ? <EditForm editIsOpen={editIsOpen} setEditIsOpen={setEditIsOpen} userInfo={userInfo.data.user}></EditForm>
-                        : <IconButton aria-label='Edit Profile' icon={<EditIcon />} onClick={handleEditButtonClick} alignSelf='end'></IconButton> : <></>}
                         <h1>{userInfo.data.user.name || userInfo.data.user.username}</h1>
                         <UserAvatar url={userInfo.data.user.avatar} name={userInfo.data.user.name}></UserAvatar>
-                        <Text color='white' bgColor='black'>{userInfo.data.user.bio || "New to bubble!"}</Text>
+                        <Text color='black' bgColor='white' border='2px' borderColor={userInfo.data.user.color}>{userInfo.data.user.bio || "New to bubble!"}</Text>
                     </Box >
-
+                    {hasEditButton ? editIsOpen ? <EditForm editIsOpen={editIsOpen} setEditIsOpen={setEditIsOpen} userInfo={userInfo.data.user}></EditForm>
+                        : <IconButton aria-label='Edit Profile' backgroundColor='white' icon={<EditIcon  className='button-size'/>} onClick={handleEditButtonClick} alignSelf='end'></IconButton> : <></>}
                     <h2>Recent Bubbles:</h2>
                     {hasEditButton ? posts.map((post) => {
                         return (
