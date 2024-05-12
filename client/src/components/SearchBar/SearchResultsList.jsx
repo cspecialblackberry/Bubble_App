e80cf020d7827ee136ce6637b5b53addf1e1d54d
Hall Family <charliesamhall@icloud.com>
2024-05-12 09:27:00 -0400

    fixed key on search results

--- client/src/components/SearchBar/SearchResultsList.jsx
+++ client/src/components/SearchBar/SearchResultsList.jsx
@@ -2,12 +2,11 @@ import "./style.css";
 import { SearchResult } from "./SearchResult";
 
 export const SearchResultsList = ({ results }) => {
-  console.log(results)
   return (
     <div className="results-list">
       {results && results.length == 0 && <p className="no-users">No users found</p>}
-      {results.map((result, id) => {
-        return <SearchResult result={result.username} id={result._id} key={id} />;
+      {results.map((result) => {
+        return <SearchResult result={result.username} id={result._id} key={result._id} />;
       })}
     </div>
   );
